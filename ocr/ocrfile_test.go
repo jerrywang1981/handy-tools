88667d1fa489f0453e6b23d1a3b0377375710c02
Jerry Wang <jianjunw@cn.ibm.com>
2020-05-12 22:13:47 +0800

    fix

---
+++ ocr/ocrfile_test.go
@@ -0,0 +1,15 @@
+package ocr
+
+import (
+	"testing"
+)
+
+func TestRecognizeFolder(t *testing.T) {
+	/*
+		fileName := "/home/ocr/result1"
+		results := RecognizeFolder(fileName, "chi_sim")
+		for _, result := range results {
+			fmt.Println(result.Text)
+		}
+	*/
+}
